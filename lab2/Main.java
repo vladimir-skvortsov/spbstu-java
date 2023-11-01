69b26866156f6567954783c2644fcbc7b4ef6ea3
Vladimir Skvortsov <mail@vladimirskvortsov.com>
2023-11-01 19:42:58 +0300

    Add lab2

---
+++ lab2/Main.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    String dir = System.getProperty("user.dir");
+    System.out.println(dir);
+
+    try {
+      Scanner fin = new Scanner(Paths.get(dir, "input.txt"), StandardCharsets.UTF_8);
+
+      while (fin.hasNext()) {
+        String word = fin.next();
+        System.out.println("Word: " + word);
+      }
+    } catch (Exception e) {
+      System.out.println(e);
+    }   
+  };
+};
